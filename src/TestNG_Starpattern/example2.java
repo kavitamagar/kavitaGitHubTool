609d34764ec8d75884287cf5a7c826a9d6e52538
magar <magar@SHIV-SHAMBHOC-M>
2024-07-05 00:30:28 +0530

    [kavita patil] :  example 2

---
+++ src/TestNG_Starpattern/example2.java
@@ -0,0 +1,10 @@
+package TestNG_Starpattern;
+
+public class example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("------kavita patil------");
+	}
+
+}
